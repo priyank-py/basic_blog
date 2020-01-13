ec9d0ab39cbf29034f88cec203a16e70275963e3
Admin <Admin>
2020-01-13 16:50:59 +0530

    blog title image added

--- articles/models.py
+++ articles/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+def upload_to(instance, filename):
+    return f'{instance.writer.username}/{filename}'
+
+
 class Article(models.Model):
 
     GENRES = (
@@ -15,6 +19,7 @@ class Article(models.Model):
 
     title  = models.CharField(max_length=120)
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title_image = models.ImageField(upload_to=upload_to,  blank=True, null=True)
     published_on = models.DateField(default=timezone.now, blank=True, null=True)
     modified_on = models.DateField(default=timezone.now, blank=True, null=True)
     genre = models.CharField(max_length=50, choices=GENRES, blank=True, null=True)
